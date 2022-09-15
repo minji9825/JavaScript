fd160c0cac07f466358884c6f8aab393ca4e2b3c
minji9825 <110438246+minji9825@users.noreply.github.com>
2022-09-15 16:07:08 +0900

    Add files via upload

---
+++ myarray.js
@@ -0,0 +1,28 @@
+const array = [1, 2, 3, 4, 5]
+
+console.log(`배열 : ${ array }`)
+console.log(`배열 : ${ 2 + 3 }`)
+
+//객체배열
+let objects = [
+    {
+        name: '멍멍이'
+    },
+    {
+        name: "야옹이"
+    }
+
+]// 갯수가 2개인 오브젝트(object)배열
+console.log(`객체 ${ objects }`)
+objects = [] //const 키워드는 창고대여업체
+objects = [0] = 20000
+console.log(
+    "객체배열의 갯수 = ",
+    objects.length
+)
+console.log(
+    "0번째 요소 = ",
+    objects [0]
+
+)
+//프로그레시브 웹기술 (progressive web)
